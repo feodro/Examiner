35418a63a74499d47437b6f989084f8b02055cc7
Denis001 <Denis00-1@yandex.ru>
2022-04-23 08:42:28 +0300

    another orm tests

--- data/db_session.py
+++ data/db_session.py
@@ -1,6 +1,5 @@
-import sqlalchemy as sa
-import sqlalchemy.orm as orm
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 import sqlalchemy.ext.declarative as dec
 
 SqlAlchemyBase = dec.declarative_base()
@@ -20,8 +19,8 @@ def global_init(db_file):
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine(conn_str, echo=False)
-    __factory = orm.sessionmaker(bind=engine)
+    engine = create_engine(conn_str, echo=False)
+    __factory = sessionmaker(bind=engine)
 
     from . import __all_models
 
@@ -30,4 +29,4 @@ def global_init(db_file):
 
 def create_session() -> Session:
     global __factory
-    return __factory()
+    return __factory
