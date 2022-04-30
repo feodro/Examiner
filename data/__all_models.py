0d011c60eec1f82ac4e0a0702d0a0a5611b91b75
Denis001 <Denis00-1@yandex.ru>
2022-04-30 10:11:19 +0300

    Счёт правильных/неправильных ответов

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,3 +1,4 @@
 from . import remember_check
 from . import db_oge
 from . import db_ege
+from . import stats
---
+++ data/stats.py
@@ -0,0 +1,12 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Stats(SqlAlchemyBase):
+    __tablename__ = 'stats'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    true = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    false = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
